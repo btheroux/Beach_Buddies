59bbfd72b595a2acb8e52636da956cd5406b699f
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-05-30 17:31:46 +0200

    gemfile basics setup - cloudinary/attachinary & omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get 'users/index'
+  # resources :users, only:
 
-  get 'users/show'
+  get "/profile", to: 'profile#index'
 
-  get 'users/destroy'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530135425) do
+ActiveRecord::Schema.define(version: 20170530151721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,12 @@ ActiveRecord::Schema.define(version: 20170530135425) do
     t.string   "gender"
     t.string   "usual_court_address"
     t.integer  "team_id"
+    t.string   "in_outdoors"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["team_id"], name: "index_users_on_team_id", using: :btree
