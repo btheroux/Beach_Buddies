be5e4048dd0a1fe1b1ba98eff39eea51222aadc9
LiviaSebok <livia.sebok12@gmail.com>
2017-05-31 16:36:19 +0200

    fixed views and fb sign in and regular sign in and some other minor things

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :countries, through: :federations
   belongs_to :team
 
-  validates :email, presence: true, uniqueness: true
+  #validates :email, presence: true, uniqueness: true
   #validates :first_name, presence: true
   #validates :last_name, presence: true
   #validates :level, presence: true
