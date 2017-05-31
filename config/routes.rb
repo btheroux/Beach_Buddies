1e8879b525ada32f10ff715db9bf9fe890813e2e
Matringe <hadrien.matringe@gmail.com>
2017-05-31 20:57:53 +0200

    created components page and basic formatting for pages to get along with the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get "/profile", to: 'profile#index'
 
   root to: 'pages#home'
+  get '/components', to: 'pages#components'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount Attachinary::Engine => "/attachinary"
