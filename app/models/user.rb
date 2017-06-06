4574f9bddd26944a20740b7fc5904ca989759e59
Matringe <hadrien.matringe@gmail.com>
2017-06-06 15:53:54 +0200

    geocoder installed and nothing seems broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_attachments :photos, maximum: 20
   before_create :default_level
+  geocoded_by :usual_court_address
+  after_validation :geocode, if: :usual_court_address_changed?
 
   has_many :videos, dependent: :destroy
   has_many :results, dependent: :destroy
