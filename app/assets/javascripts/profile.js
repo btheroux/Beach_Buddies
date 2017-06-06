8ebf21b52cb96a9dc3ee6938d5e87ef4361fccfe
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-06-06 10:41:23 +0200

    javascript & jquery

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -9,4 +9,37 @@ $(document).ready(function (){
       createTeamForm.hide();
     });
   });
+  var createRankingButton = $('#create-ranking')
+  var createRankingForm = $('#ranking-create-form')
+  var rankingCancelButton = $('<a href="#" id="ranking-cancel-button" class="btn btn-danger">Cancel</a> ')
+  createRankingButton.on("click", function () {
+    console.log("It worked!")
+    createRankingForm.show();
+    createRankingForm.append(rankingCancelButton)
+    rankingCancelButton.on("click", function () {
+      createRankingForm.hide();
+    });
+  });
+  var createResultButton = $('#create-result')
+  var createResultForm = $('#result-create-form')
+  var resultCancelButton = $('<a href="#" id="result-cancel-button" class="btn btn-danger">Cancel</a> ')
+  createResultButton.on("click", function () {
+    console.log("It worked!")
+    createResultForm.show();
+    createResultForm.append(resultCancelButton)
+    resultCancelButton.on("click", function () {
+      createResultForm.hide();
+    });
+  });
+  var createVideoButton = $('#create-video')
+  var createVideoForm = $('#video-create-form')
+  var videoCancelButton = $('<a href="#" id="video-cancel-button" class="btn btn-danger">Cancel</a> ')
+  createVideoButton.on("click", function () {
+    console.log("It worked!")
+    createVideoForm.show();
+    createVideoForm.append(videoCancelButton)
+    videoCancelButton.on("click", function () {
+      createVideoForm.hide();
+    });
+  });
 });
