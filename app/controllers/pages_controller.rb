8c195398713cde08e42927399fe9d12e8a3a7c61
Lars Pedersen <lpcool@gmail.com>
2017-06-09 15:44:49 +0200

    golden-button2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,10 @@ class PagesController < ApplicationController
 
   def home
     if current_user
-      @recommended_user = User.where(level: current_user.level, gender: current_user.gender).sample
-    else
-      @recommended_user = User.all.sample
+      @recommended_user = User.where.not(id: current_user.id).where(level: current_user.level, gender: current_user.gender).sample
     end
+
+    @recommended_user ||= User.all.sample
   end
 
   def resultbox
